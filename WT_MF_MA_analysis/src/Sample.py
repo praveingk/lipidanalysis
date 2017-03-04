a663d5de463369310e7a0be861e4b7d9626654ea
Pravein GK <praveingk@gmail.com>
2017-03-04 15:30:22 +0800

    abundance calc

--- WT_MF_MA_analysis/src/Sample.py
+++ WT_MF_MA_analysis/src/Sample.py
@@ -11,26 +11,31 @@ class Sample:
 
     def __init__(self, name):
         self.name = name
-        self.lipids = []
+        self.lipids = {}
         self.normalizedToIS = {}
+        self.lipidAbundance = {}
+        self.lipidSum = 0
 
-    def addLipid(self, lipidVal):
-        self.lipids.append(lipidVal)
+    def addLipid(self, lipidName, lipidVal):
+        self.lipids[lipidName] = lipidVal
 
-    def getLipidVal(self, index):
-        return self.lipids[index]
+    def getLipidVal(self, lipidName):
+        return self.lipids[lipidName]
 
     def toStr(self):
         return self.name +" : "+ str(self.lipids)
 
-    def calcNormal(self, lipid1Index, lipid2Index, concetration):
-        l1Val = self.lipids[lipid1Index]
-        l2Val = self.lipids[lipid2Index]
+    def getName(self):
+        return self.name
+
+    def calcNormal(self, lipid1, lipid2, concetration):
+        l1Val = self.lipids[lipid1]
+        l2Val = self.lipids[lipid2]
         if l2Val == 0 :
-            self.normalizedToIS[lipid1Index] = 0
+            self.normalizedToIS[lipid1] = 0
             return
         normalized = float((l1Val/l2Val) * concetration)
-        self.normalizedToIS[lipid1Index] = normalized
+        self.normalizedToIS[lipid1] = normalized
         print "Normalized " + str(normalized)
         pass
 
@@ -38,4 +43,24 @@ class Sample:
         return self.name
 
     def getNormal(self):
-        return self.normalizedToIS.itervalues()
+        return self.normalizedToIS
+
+    def getNormalHeading(self, lipidIndexMap):
+        line = ""
+        for i in range(0,len(lipidIndexMap)):
+        #for l in self.normalizedToIS.iterkeys():
+            line += lipidIndexMap[i]+","
+        return line
+
+    def calcRelAbundance(self, lipidIndicator):
+        for lipid  in lipidIndicator:
+            if lipidIndicator[lipid] == 1 :
+                self.lipidSum += self.lipids[lipid]
+        for lipid in lipidIndicator:
+            if lipidIndicator[lipid] == 1:
+                self.lipidAbundance[lipid] = (self.lipids[lipid] / self.lipidSum) * 100
+
+    def getLipidAbundance(self):
+        return self.lipidAbundance
+
+
--- WT_MF_MA_analysis/src/maAnalyze.py
+++ WT_MF_MA_analysis/src/maAnalyze.py
@@ -13,99 +13,39 @@ import time
 import pickle, pprint
 
 # Helper Methods
+from src.Run import Run
 from src.Sample import Sample
 
 
-def loadLipidIndices(lineSplit):
-    i = 1
-    for lipid in lineSplit:
-        if i == len(lineSplit):
-            break
-        lipid = lineSplit[i].rstrip()
-        lipidIndexMap[i-1] = lipid
-        lipidIndexRevMap[lipid] = i-1
-        print str(i-1)+"," + lipid
-        i=i+1
-
-def loadSamples(lineSplit):
-    i = 0
-    for item in lineSplit:
-        if i == 0:
-            S = Sample(item)
-            Samples.append(S)
-            print "Creating "+ S.toStr()
-        else:
-            S.addLipid(float(item))
-        i=i+1
-    print S.toStr()
-
-def loadThymus():
-    if not os.path.exists(thymusFile):
-        print "Thymus File not Found!!!"
-        return 1
-    thymus = open(thymusFile, "r")
-    firstLine = True
-    for line in thymus.readlines():
-        lineSplit = line.split(",")
-        if firstLine:
-            loadLipidIndices(lineSplit)
-            firstLine = False
-        else :
-            loadSamples(lineSplit)
-    thymus.close()
-    pass
-
-def processNormal():
-    if not os.path.exists(normalFile):
-        print "Normal File not Found!!!"
-        return 1
-    normal = open(normalFile, "r")
-    firstLine = True
-    for line in normal.readlines():
-        if firstLine:
-            firstLine = False
-            continue
-        lineSplit = line.split(",")
-        lipid = lineSplit[0]
-        std = lineSplit[1]
-        concentration = float(lineSplit[2])
-        print lipid
-        lipidIndex = lipidIndexRevMap[lipid]
-        stdIndex = lipidIndexRevMap[std]
-
-        for s in Samples:
-            s.calcNormal(lipidIndex, stdIndex, concentration)
-        normal.close()
-    pass
 
 
-def exportNormalized():
-    normalized = open("Output.csv", "w")
-
-    for s in Samples:
-        line = s.getName()+","
-        normalVals = s.getNormal()
-        for n in normalVals:
-            line = line + str(n)+","
-        normalized.write(line+"\n")
-    normalized.close()
-    pass
 
 # Start Main
 
 if 4 > len(sys.argv):
-    print 'Usage : maAnalyze <Thymus File> <Normal File> <Command Start/Stop/etc>'
+    print 'Usage : maAnalyze <Input Files> <Output Directory> <Command Start/Stop/etc>'
+    print "Input Files : \n" \
+          " RunFile.csv, normalFile.csv\n" \
+          "...\n" \
+          "...\n"
+    print 'Usage : maAnalyze <Run Files> <Normal File> <Command Start/Stop/etc>'
     sys.exit(1)
 
 # Initialize some basic parameter files
-thymusFile = str(sys.argv[1])
-normalFile = str(sys.argv[2])
+inputFile = str(sys.argv[1])
+outputDir = str(sys.argv[2])
 command = str(sys.argv[3])
-lipidIndexMap = {}
-lipidIndexRevMap = {}
-Samples = []
+print command
 if command.lower() == 'start':
-    loadThymus()
-    print str(lipidIndexRevMap)
-    processNormal()
-    exportNormalized()
+    inF = open(inputFile, "r")
+    for line in inF.readlines():
+        lineSplit = line.split(",")
+        runFile = lineSplit[0]
+        normalFile = lineSplit[1]
+        # Do this repeatedly
+        run = Run(runFile)
+        run.loadRun(runFile)
+        run.processNormal(normalFile)
+        run.exportNormalized(outputDir+"/putput.csv")
+        run.calcRelAbundance()
+        run.exportLipidAbundance(outputDir+"/abundance.csv")
